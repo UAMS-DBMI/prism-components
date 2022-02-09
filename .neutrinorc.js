b0d898ee9b6690a076153fd184295ee8e6a91996
Joseph Utecht <jrutecht@uams.edu>
2022-02-09 15:32:08 -0600

    proxy config...ing

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,14 +1,24 @@
 const standard = require('@neutrinojs/standardjs');
 const reactComponents = require('@neutrinojs/react-components');
 const mocha = require('@neutrinojs/mocha');
+const web = require('@neutrinojs/web');
 
 module.exports = {
   options: {
     root: __dirname,
+    devServer: {
+      proxy: {
+        '/api': {
+          target: 'http://sui-demo-prism.apps.dbmi.cloud',
+          pathRewrite: { '^/api': '' },
+          changeOrigin: true
+        }
+      }
+    }
   },
   use: [
     standard(),
     reactComponents(),
-    mocha(),
+    mocha()
   ],
 };
