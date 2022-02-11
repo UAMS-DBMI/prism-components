73420040f60ccd6598fbfe2a60973643a1b66f22
Joseph Utecht <thenmal@gmail.com>
2022-02-11 07:16:16 -0600

    inserted example of inserting custom api

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,19 @@ import { render } from 'react-dom'
 import Example from './components/Example'
 import CohortBuilderComponent from './components/CohortBuilder'
 
+const testFetch = async (url, opts = {}) => {
+  const apiOpts = {
+    headers: new Headers({
+      Authorization: 'Basic ' + btoa('username:password')
+    })
+  }
+  opts = Object.assign(opts, apiOpts)
+  const response = await fetch(url, opts)
+  return await response.json()
+}
+
 render(
   <div style={{ display: 'flex', flexDirection: 'column' }}>
     <Example />
-    <CohortBuilderComponent />
+    <CohortBuilderComponent api={testFetch} />
   </div>, document.getElementById('root'))
